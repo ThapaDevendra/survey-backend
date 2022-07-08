247559c5ee4be5180912b6a78e1bfbc6d8953314
ThapaDevendra <43578948+ThapaDevendra@users.noreply.github.com>
2022-07-08 12:16:02 -0500

    Update prod.db.config.js

--- app/config/prod.db.config.js
+++ app/config/prod.db.config.js
@@ -1,36 +1,14 @@
-<Proxy *>
-        Order deny,allow
-        Allow from all
-</Proxy>
-ProxyRequests Off
-ProxyPass /api http://localhost:3000/api
-<VirtualHost *:80>
-        # The ServerName directive sets the request scheme, hostname and port that
-        # the server uses to identify itself. This is used when creating
-        # redirection URLs. In the context of virtual hosts, the ServerName
-        # specifies what hostname must appear in the request's Host: header to
-        # match this virtual host. For the default virtual host (this file) this
-        # value is not decisive as it is used as a last resort host regardless.
-        # However, you must set it for any further virtual host explicitly.
-        #ServerName www.example.com
-
-        ServerAdmin webmaster@localhost
-        DocumentRoot /var/www/html
-
-        # Available loglevels: trace8, ..., trace1, debug, info, notice, warn,
-        # error, crit, alert, emerg.
-        # It is also possible to configure the loglevel for particular
-        # modules, e.g.
-        #LogLevel info ssl:warn
-
-        ErrorLog ${APACHE_LOG_DIR}/error.log
-        CustomLog ${APACHE_LOG_DIR}/access.log combined
-
-        # For most configuration files from conf-available/, which are
-        # enabled or disabled at a global level, it is possible to
-        # include a line for only one particular virtual host. For example the
-        # following line enables the CGI configuration for this host only
-        # after it has been globally disabled with "a2disconf".
-        #Include conf-available/serve-cgi-bin.conf
-</VirtualHost>
+module.exports = {
+    HOST: 'localhost',
+    USER: 'root',
+    PASSWORD: 'Un!versity2022',
+    DB: 'survey',
+    dialect: 'mysql', 
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
 
