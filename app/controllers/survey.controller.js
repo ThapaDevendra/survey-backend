950a2866f9f31040f8df16086d550a00c9780393
ThapaDevendra <43578948+ThapaDevendra@users.noreply.github.com>
2022-07-27 22:01:40 -0500

    Update survey.controller.js

--- app/controllers/survey.controller.js
+++ app/controllers/survey.controller.js
@@ -1,6 +1,6 @@
 const db = require("../models/index.js");
 const Survey = db.surveys;
-// const Question = db.questions;
+const Question = db.questions;
 
 exports.create = async (req, res) => {
   if (!req.body.name) {
@@ -21,8 +21,7 @@ exports.create = async (req, res) => {
 };
 
 exports.getAll = async (req, res) => {
-  const userID = req.params.userID;
-  await Survey.findAll({where: {userID: userID}})
+  await Survey.findAll({}})
     .then((data) => {
       res.send(data);
     })
@@ -35,17 +34,10 @@ exports.getAll = async (req, res) => {
 
 
 exports.findOne = async (req, res) => {
-  await getSingleSurvey(req.params.surveyID)
+  await getSingleSurvey(req.params.id)
     .then((data) => {
-      if(!data)
-      {
-        res.status(404).send({
-          message: `Survey with surveyID: ${req.params.surveyID} do not exist`
-        })
-      }else{
-        res.status(200).send(data);
-        return;
-      }
+      res.status(200).send(data);
+        return;      
     })
     .catch((err) => {
       console.log(">> Error while finding survey: ", err);
@@ -53,7 +45,6 @@ exports.findOne = async (req, res) => {
 };
 
 exports.delete = (req, res) => {
-  const id = req.params.surveyID;
   Survey.destroy({
     where: { id: id },
   })
@@ -75,52 +66,24 @@ exports.delete = (req, res) => {
     });
 };
 
-exports.update = async (req, res) => {
-  const surveyID = req.params.surveyID;
-  if(!surveyID){
-    res.status(400).send({
-      message: 'User survey ID can not be empty!!'
 
-    })
-    return;
-  }
-  await getSingleSurvey(surveyID).then(data =>{
-      Survey.update(req.body, {where: {id: surveyID }}).then(num => {
-        if(num == 1){
-          res.send({
-            message: 'Survey name updated successfully.'
-          });
-        }else{
-          res.send({
-            message: `Cannot update Survey with id=${surveyID}`
-
-          })
-        }
-      }).catch(err =>{
-        res.status(500).send({
-          message: err.message + 'with id: ' + surveyID
-        })
-      })
-    })
+function getSingleSurvey(id) {
+  return Survey.findByPk(id, { include: [] });
 }
 
-function getSingleSurvey(surveyID) {
-  return Survey.findByPk(surveyID, { include: [] });
-}
-
-// exports.getSurveyQuestions = async (req, res) => {
-//   const id = req.params.surveyId;
-//   const data = await Survey.findOne({
-//     include: [
-//       {
-//         model: Question,
-//         as: "questions",
-//       },
-//     ],
-//     where: { id: id },
-//   });
-//   res.status(200).send(data);
-// };
+ exports.getSurveyQuestions = async (req, res) => {
+   const id = req.params.surveyId;
+   const data = await Survey.findOne({
+     include: [
+       {
+         model: Question,
+         as: "questions",
+       },
+     ],
+     where: { id: id },
+   });
+   res.status(200).send(data);
+ };
 
 
 
