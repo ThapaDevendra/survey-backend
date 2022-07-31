119c754642b5f4d9874b645a4d198b0dde9e9222
ThapaDevendra <43578948+ThapaDevendra@users.noreply.github.com>
2022-07-31 09:34:02 -0500

    Update survey.routes.js

--- app/routes/survey.routes.js
+++ app/routes/survey.routes.js
@@ -1,13 +1,13 @@
-module.exports = (app) => {
-  const survey = require("../controllers/survey.controller.js");
-  var router = require("express").Router();
+module.exports = app => {
+    const survey = require('../controllers/survey.controller.js');
+    var router = require('express').Router();
 
-  router.post("/", survey.create);
-  router.get("/getAllSurveys/:userID", survey.getAll);
-  router.get("/:surveyID", survey.findOne);
-  router.delete("/:surveyID", survey.delete);
-  router.put("/:surveyID", survey.update);
-  router.get("/search/:name", survey.findByName);
+    router.post('/', survey.create); 
+    router.get('/getAllSurveys/:userID', survey.getAll); 
+    router.get('/:surveyID', survey.findOne); 
+    router.delete('/:surveyID', survey.delete);
+    router.put('/:surveyID', survey.update)
+    router.get("/search/:name", survey.findByName);
 
   app.use("/api/surveys", router);
 };
