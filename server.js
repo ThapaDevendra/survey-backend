9af1625258f1c647684f684160c2f3e5a7bf48f8
ThapaDevendra <43578948+ThapaDevendra@users.noreply.github.com>
2022-08-06 18:12:15 -0500

    Update server.js

--- server.js
+++ server.js
@@ -16,12 +16,12 @@ app.use(express.urlencoded({ extended: true }));
 
 const db = require("./app/models/index.js");
 
-//db.sequelize.sync();
+db.sequelize.sync();
 
 //for devel to recreate each time database
-db.sequelize.sync({ force: true }).then(() => {
-  console.log("Drop and re-sync db.");
-});
+// db.sequelize.sync({ force: true }).then(() => {
+//   console.log("Drop and re-sync db.");
+// });
 
 app.get("/", (req, res) => {
   res.send("<h2> Welcome to Survey Backend! <h2>");
